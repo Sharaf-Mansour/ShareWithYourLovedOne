534f28690837a1d774f6a4f3001c998da14bc383
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2025-01-15 22:44:09 +0200

    STORAGES: Book

---
+++ Brokers/Storages/IStorageBroker.Book.cs
@@ -0,0 +1,10 @@
+﻿using library.Models;
+namespace library.Brokers.Storages;
+public partial interface IStorageBroker
+{
+    ValueTask InsertBookAsync(Book book);
+    ValueTask<List<Book>> SelectAllBooksAsync();
+    ValueTask<Book> SelectBookByIdAsync(int bookId);
+    ValueTask UpdateBookAsync(Book book);
+    ValueTask DeleteBookAsync(int bookId);
+}
---
+++ Brokers/Storages/StorageBroker.Book.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using library.Models;
+namespace library.Brokers.Storages;
+public partial class StorageBroker : IStorageBroker
+{
+    public async ValueTask InsertBookAsync(Book book)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("INSERT INTO Book(book_id,title) VALUES (@book_id, @title)",book);
+    }
+    public async ValueTask<List<Book>> SelectAllBooksAsync()
+    {
+        using var connection = CreateConnection();
+        return (await connection.QueryAsync<Book>("SELECT * FROM Book")).ToList();
+    }
+    public async ValueTask<Book> SelectBookByIdAsync(int bookId)
+    {
+        using var connection = CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<Book>("SELECT * FROM Book WHERE book_id = @book_id", new { book_id = bookId });
+    }
+    public async ValueTask UpdateBookAsync(Book book)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("UPDATE Book SET title = @title WHERE book_id = @book_id", book);
+    }
+    public async ValueTask DeleteBookAsync(int bookId)
+    {
+        using var connection = CreateConnection();
+        await connection.ExecuteAsync("DELETE FROM Book WHERE book_id = @book_id", new { book_id = bookId });
+    }
+}
