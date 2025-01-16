2647bd6cc36387d52bf57a75958cb43797d3dfd2
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2025-01-16 19:28:01 +0200

    CodeRub

--- Controllers/ControllersExtentions.AuthorController.cs
+++ Controllers/ControllersExtentions.AuthorController.cs
@@ -6,48 +6,48 @@ public partial class ControllersExtentions
     {
         var groupName = "Authors";
 
-        app.MapGet("/api/Authors", GetAllAuthorsAsync)
+        app.MapGet("/api/authors", GetAllAuthorsAsync)
             .WithTags(groupName)
             .WithSummary(nameof(GetAllAuthorsAsync))
             .WithDescription(
             """
-            Retrieves a list of all user Authors in the system. This endpoint returns comprehensive details of each Author.
+            Retrieves a list of all user authors in the system. This endpoint returns comprehensive details of each author.
             """
             ).Produces<Author[]>(200);
 
-        app.MapGet("/api/Authors/{id}", GetAuthorByIdAsync)
+        app.MapGet("/api/authors/{id}", GetAuthorByIdAsync)
             .WithTags(groupName)
             .WithSummary(nameof(GetAuthorByIdAsync))
             .WithDescription(
             """
-            Fetches the details of a specific user Author based on the provided Author ID.
+            Fetches the details of a specific user author based on the provided author ID.
             """
             ).Produces<Author>(200).Produces(204).ProducesProblem(400);
 
-        app.MapPost("/api/Authors", CreateAuthorAsync)
+        app.MapPost("/api/authors", CreateAuthorAsync)
             .WithTags(groupName)
             .WithSummary(nameof(CreateAuthorAsync))
             .WithDescription(
             """
-            Creates a new user Author with the provided information. Ensure that the data meets the required validation criteria.
+            Creates a new user author with the provided information. Ensure that the data meets the required validation criteria.
             """
             ).Produces<Author>(201).ProducesProblem(400);
 
-        app.MapPut("/api/Authors/{id}", UpdateAuthorAsync)
+        app.MapPut("/api/authors/{id}", UpdateAuthorAsync)
             .WithTags(groupName)
             .WithSummary(nameof(UpdateAuthorAsync))
             .WithDescription(
             """
-            Updates the details of an existing user Author identified by the given Author ID. Only authorized users can perform this action.
+            Updates the details of an existing user author identified by the given author ID. Only authorized users can perform this action.
             """
             );
 
-        app.MapDelete("/api/Authors/{id}", DeleteAuthorAsync)
+        app.MapDelete("/api/authors/{id}", DeleteAuthorAsync)
             .WithTags(groupName)
             .WithSummary(nameof(DeleteAuthorAsync))
             .WithDescription(
             """
-            Deletes a user Author based on the provided Author ID. This action is irreversible and should be performed with caution.
+            Deletes a user author based on the provided author ID. This action is irreversible and should be performed with caution.
             """
             );
         return app;
