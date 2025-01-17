d2c0315ded137807bb44dbe3c5813cba527721d2
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2025-01-17 15:58:18 +0200

    CODERUB

--- Foundation/Services/AuthorService.cs
+++ Foundation/Services/AuthorService.cs
@@ -9,24 +9,4 @@ public class AuthorService (IStorageBroker storageBroker): IAuthorService
     public async ValueTask<Author?> RetrieveAuthorByIdAsync(int author_id) => await storageBroker.SelectAuthorByIdAsync(author_id);
     public async ValueTask ModifyAuthorAsync(Author author) => await storageBroker.UpdateAuthorAsync(author);
     public async ValueTask RemoveAuthorByIdAsync(int author_id) => await storageBroker.DeleteAuthorAsync(author_id);
-
-    //public ValueTask AddAuthorAsync(Author author)
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //ValueTask<List<Author>> IAuthorService.RetrieveAllAuthorsAsync()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //ValueTask<Author?> IAuthorService.RetrieveAuthorByIdAsync(int author_id)
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //public ValueTask ModifyAuthorAsync(Author author)
-    //{
-    //    throw new NotImplementedException();
-    //}
 }
