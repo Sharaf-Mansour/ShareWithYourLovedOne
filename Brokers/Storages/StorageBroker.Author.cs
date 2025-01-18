efd40e0c3767a90f2457e914a8f435f7944744aa
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2025-01-18 21:21:44 +0200

    Nuked _id

--- Brokers/Storages/StorageBroker.Author.cs
+++ Brokers/Storages/StorageBroker.Author.cs
@@ -10,21 +10,21 @@ public partial class StorageBroker : IStorageBroker
     public async ValueTask<IEnumerable<Author>> SelectAllAuthorsAsync()
     {
         using var connection = CreateConnection();
-        return (await connection.QueryAsync<Author>("SELECT * FROM Author")).AsList();
+        return (await connection.QueryAsync<Author>("SELECT * FROM Author"));
     }
-    public async ValueTask<Author?> SelectAuthorByIdAsync(int author_id)
+    public async ValueTask<Author?> SelectAuthorByIdAsync(int Id)
     {
         using var connection = CreateConnection();
-        return await connection.QueryFirstOrDefaultAsync<Author>("SELECT * FROM Author WHERE Id = @author_id", new {  author_id });
+        return await connection.QueryFirstOrDefaultAsync<Author>("SELECT * FROM Author WHERE Id = Id", new { Id });
     }
     public async ValueTask UpdateAuthorAsync(Author author)
     {
         using var connection = CreateConnection();
         await connection.ExecuteAsync("UPDATE Author SET Name = @Name WHERE Id = @Id", author);
     }
-    public async ValueTask DeleteAuthorAsync(int author_id)
+    public async ValueTask DeleteAuthorAsync(int Id)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("DELETE FROM Author WHERE Id = @author_id", new { author_id });
+        await connection.ExecuteAsync("DELETE FROM Author WHERE Id = @Id", new { Id });
     }
 }
