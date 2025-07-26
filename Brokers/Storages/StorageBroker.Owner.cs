490004e312cec2f3d8014d660963e60dfd54edd1
Steven Nagy <stevennagy788@gmail.com>
2025-07-26 21:19:25 +0300

    Update StorageBroker.Owner.cs
    making sure using same letter casing for simplicity

--- Brokers/Storages/StorageBroker.Owner.cs
+++ Brokers/Storages/StorageBroker.Owner.cs
@@ -14,10 +14,10 @@ public partial class StorageBroker : IStorageBroker
         using var connection = CreateConnection();
         return (await connection.QueryAsync<Owner>("SELECT * FROM Owner"));//, owner_id as Id, route_token as RouteToken
     }
-    public async ValueTask<Owner?> SelectOwnerByIdAsync(int Id)
+    public async ValueTask<Owner?> SelectOwnerByIdAsync(int ID)
     {
         using var connection = CreateConnection();
-        return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT * FROM Owner WHERE ID = @Id", new { Id });
+        return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT * FROM Owner WHERE ID = @ID", new { ID });
     }
     public async ValueTask<Owner?> SelectOwnerByEmailAsync(string Email)
     {
@@ -27,16 +27,16 @@ public partial class StorageBroker : IStorageBroker
     public async ValueTask UpdateOwnerAsync(Owner owner)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("UPDATE Owner SET Name = @Name, Email = @Email, Password = @Password WHERE ID = @Id", owner);
+        await connection.ExecuteAsync("UPDATE Owner SET Name = @Name, Email = @Email, Password = @Password WHERE ID = @ID", owner);
     }
     public async ValueTask<Owner?> SelectOwnerByRouteTokenAsync(string routeToken)
     {
         using var connection = CreateConnection();
         return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT ID, name, email, RouteToken FROM Owner WHERE RouteToken = @routeToken", new { routeToken });
     }
-    public async ValueTask DeleteOwnerAsync(int Id)
+    public async ValueTask DeleteOwnerAsync(int ID)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("DELETE FROM Owner WHERE ID = @Id", new { Id });
+        await connection.ExecuteAsync("DELETE FROM Owner WHERE ID = @ID", new { ID });
     }
 }
