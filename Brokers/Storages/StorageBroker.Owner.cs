97dc92bcc672717a6993a87252a94726404a4d48
Steven Nagy <stevennagy788@gmail.com>
2025-07-28 21:08:15 +0300

    updating broker naming for consistency and editing nullability to remove warnings

--- Brokers/Storages/StorageBroker.Owner.cs
+++ Brokers/Storages/StorageBroker.Owner.cs
@@ -1,38 +1,35 @@
-﻿using Library.Models;
-using System;
-
-namespace Library.Brokers.Storages;
+﻿namespace Library.Brokers.Storages;
 public partial class StorageBroker : IStorageBroker
 {
-    public async ValueTask InsertOwnerAsync(Owner owner)
+    public async ValueTask InsertOwnerAsync(Owner Owner)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("INSERT INTO Owner(Name, Email, Password, RouteToken) VALUES (@Name, @Email, @Password, @RouteToken )", owner);
+        await connection.ExecuteAsync("INSERT INTO Owner(Name, Email, Password, RouteToken) VALUES (@Name, @Email, @Password, @RouteToken )", Owner);
     }
     public async ValueTask<IEnumerable<Owner>> SelectAllOwnersAsync()
     {
         using var connection = CreateConnection();
-        return (await connection.QueryAsync<Owner>("SELECT * FROM Owner"));//, owner_id as Id, route_token as RouteToken
+        return await connection.QueryAsync<Owner>("SELECT * FROM Owner");
     }
     public async ValueTask<Owner?> SelectOwnerByIdAsync(int ID)
     {
         using var connection = CreateConnection();
         return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT * FROM Owner WHERE ID = @ID", new { ID });
     }
-    public async ValueTask<Owner?> SelectOwnerByEmailAsync(string Email)
+    public async ValueTask<Owner?> SelectOwnerByEmailAsync(string? Email)
     {
         using var connection = CreateConnection();
         return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT * FROM Owner WHERE Email = @Email", new { Email });
     }
-    public async ValueTask UpdateOwnerAsync(Owner owner)
+    public async ValueTask UpdateOwnerAsync(Owner Owner)
     {
         using var connection = CreateConnection();
-        await connection.ExecuteAsync("UPDATE Owner SET Name = @Name, Email = @Email, Password = @Password WHERE ID = @ID", owner);
+        await connection.ExecuteAsync("UPDATE Owner SET Name = @Name, Email = @Email, Password = @Password WHERE ID = @ID", Owner);
     }
-    public async ValueTask<Owner?> SelectOwnerByRouteTokenAsync(string routeToken)
+    public async ValueTask<Owner?> SelectOwnerByRouteTokenAsync(string RouteToken)
     {
         using var connection = CreateConnection();
-        return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT ID, name, email, RouteToken FROM Owner WHERE RouteToken = @routeToken", new { routeToken });
+        return await connection.QueryFirstOrDefaultAsync<Owner>("SELECT ID, name, email, RouteToken FROM Owner WHERE RouteToken = @RouteToken", new { RouteToken });
     }
     public async ValueTask DeleteOwnerAsync(int ID)
     {
