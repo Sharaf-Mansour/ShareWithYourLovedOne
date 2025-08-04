0a45d48bd71c91cd02131aa7965a05de9d8408a5
Steven Nagy <stevennagy788@gmail.com>
2025-08-04 19:22:33 +0300

    splitting the code from the tags/ui to be more clean for me

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,79 @@
+﻿namespace Library.Components.Pages
+{
+    public partial class Home
+    {
+        private bool isLoggedIn = false;
+        private bool toggel = false;
+        private Owner owner = new();
+        private Owner loggedInOwner = new();
+        private string? errorMessage;
+        private ScheduleEntry entry = new();
+        private List<ScheduleEntry> scheduleEntries = [];
+        private DateOnly Date;
+        private TimeOnly startTime ;
+        private TimeOnly endTime ;
+        private async Task HandleSignUpAsync()
+        {
+            errorMessage = null;
+            try
+            {
+                if (!(string.IsNullOrWhiteSpace(owner.Name) && string.IsNullOrWhiteSpace(owner.Email)&& string.IsNullOrWhiteSpace(owner.Password)))
+                {
+                    await OwnerService.AddOwnerAsync(owner);
+                    var loggedInOwnerId = await OwnerService.LoginAsync(owner);
+                    ToLoggedInState(loggedInOwnerId);
+                }
+                    
+                errorMessage = "All entries field must be filled";
+            }
+            catch (EmailAlreadyInUse)
+            {
+                errorMessage = "Email already in use";
+            }
+        }
+        private async Task HandleLoginAsync()
+        {
+            errorMessage = null;
+            try
+            {
+                if (!(string.IsNullOrWhiteSpace(owner.Email) && string.IsNullOrWhiteSpace(owner.Password)))
+                {
+                    var loggedInOwnerId = await OwnerService.LoginAsync(owner);
+                    ToLoggedInState(loggedInOwnerId);
+                }
+                    
+                errorMessage = "All entries field must be filled";
+            }
+            catch (InvalidCredentialsException)
+            {
+                errorMessage = "Email or Password is incorrect!";
+            }
+
+        }
+        private void SwitchMode(bool changTo)
+        {
+            toggel = changTo;
+            errorMessage = null;
+            owner = new();
+        }
+        private async void ToLoggedInState(int id)
+        {
+            
+            loggedInOwner = await OwnerService.RetrieveOwnerByIdAsync(id)?? new();
+            if (loggedInOwner is not null)
+            {
+                isLoggedIn = true;
+                await LoadScheduleEntries();
+            }
+
+        }
+        private async Task LoadScheduleEntries()
+        {
+
+            scheduleEntries = (await ScheduleEntryService.RetrieveAllScheduleEntriesForOwnerAsync(loggedInOwner.ID)).ToList();
+
+        }
+
+
+    }
+}
