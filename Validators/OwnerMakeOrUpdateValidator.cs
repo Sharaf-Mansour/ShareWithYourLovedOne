b2a2a1cc2581512814b37cbaf571198caedd38cb
Steven Nagy <stevennagy788@gmail.com>
2025-08-08 23:42:20 +0300

    adding validators for owner attributes

---
+++ Validators/OwnerMakeOrUpdateValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+namespace Library.Validators
+{
+    public class OwnerMakeOrUpdateValidator : AbstractValidator<Owner>
+    {
+        public OwnerMakeOrUpdateValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Full Name cannot be empty.")
+                .MaximumLength(100).WithMessage("Name is too long.");
+
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email address cannot be empty.")
+                .EmailAddress().WithMessage("A valid email address is required.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password cannot be empty.")
+                .MinimumLength(8).WithMessage("New password must be at least 8 characters.");
+        }
+    }
+}
