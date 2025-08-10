5227cbd1e73ea9ec2c41df01b1f187ff2a2dcbc5
Steven Nagy <stevennagy788@gmail.com>
2025-08-10 14:44:58 +0300

    solving name var validation in signup by adding rules for login only

--- Validators/OwnerValidator.cs
+++ Validators/OwnerValidator.cs
@@ -1,9 +1,9 @@
 ﻿using FluentValidation;
 namespace Library.Validators
 {
-    public class OwnerMakeOrUpdateValidator : AbstractValidator<Owner>
+    public class OwnerValidator : AbstractValidator<Owner>
     {
-        public OwnerMakeOrUpdateValidator()
+        public OwnerValidator()
         {
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Full Name cannot be empty.")
@@ -17,6 +17,16 @@ namespace Library.Validators
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Password cannot be empty.")
                 .MinimumLength(8).WithMessage("New password must be at least 8 characters.");
+
+            RuleSet("Login", () =>
+            {
+                RuleFor(x => x.Email)
+                    .NotEmpty().WithMessage("Email address cannot be empty.")
+                    .EmailAddress().WithMessage("A valid email address is required.");
+
+                RuleFor(x => x.Password)
+                    .NotEmpty().WithMessage("Password cannot be empty.");
+            });
         }
     }
 }
